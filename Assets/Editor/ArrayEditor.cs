d003fbbda7d18b47aa971adb27cfe1ba062132e1
simoneliskases <simon.eliskases@outlook.de>
2021-03-13 13:38:32 +0100

    Init Unity Project & Added Arrays Script

---
+++ Assets/Editor/ArrayEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Arrays))]
+public class ArrayEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        Arrays arrays = (Arrays)target;
+        EditorGUILayout.Space();
+
+
+        if (GUILayout.Button("Calculate Sum"))
+        {
+            Debug.Log(arrays.CalculateSum());
+        }
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Calculate Output"))
+        {
+            Debug.Log(arrays.CalculateOuput());
+        }
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Create Text"))
+        {
+            Debug.Log(arrays.CreateText());
+        }
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Average Y-Position"))
+        {
+            Debug.Log(arrays.AverageYPosition());
+        }
+        EditorGUILayout.Space();
+    }
+
+}
---
+++ Assets/MyGame/Scripts/Arrays.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public float[] sumValues;
+    public bool[] boolInputs;
+    public string[] textSegments;
+    public GameObject[] objects;
+
+    public float CalculateSum()
+    {
+        float sum = 0;
+
+        foreach (var value in sumValues)
+        {
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    public bool CalculateOuput()
+    {
+        int numTrue = 0;
+        int numFalse = 0;
+
+        foreach(var input in boolInputs)
+        {
+            if (input)
+            {
+                numTrue++;
+            }
+            else
+            {
+                numFalse++;
+            }
+        }
+
+        if(numFalse > numTrue)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public string CreateText()
+    {
+        string text = null;
+
+        foreach(var segment in textSegments)
+        {
+            text += segment;
+        }
+
+        return text;
+    }
+
+    public float AverageYPosition()
+    {
+        float ySum = 0;
+
+        foreach(var entity in objects)
+        {
+            ySum += entity.transform.position.y;
+        }
+
+        return ySum / objects.Length;
+    }
+}
